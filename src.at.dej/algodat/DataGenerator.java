6283b95be11253585408bb221012d360baa6291a
julianegg <94118592+julianegg@users.noreply.github.com>
2024-12-02 11:34:08 +0100

    finished but cant run

--- src.at.dej/algodat/DataGenerator.java
+++ src.at.dej/algodat/DataGenerator.java
@@ -1,15 +1,26 @@
 public class DataGenerator {
 
     public class DataGenerator{
-        public static int[] generateDataArray(){
+        public static int[] generateDataArray(int size){
             Random random = new Random();
-            int[] data = new int[5];
+            int[] data = new int[size];
             
             for (int i = 0; i < data.length; i++) {
-                data[i] = random.nextInt(5); 
+                data[i] = random.nextInt(); 
             }
             return data; 
             
         }
+
+    }
+    public static int[] generateDataArray(int size, int min, int max){
+        Random random = new Random();
+        int[] data = new int[size];
+        
+        for (int i = 0; i < data.length; i++) {
+            data[i] = random.nextInt(min, max); 
+        }
+        return data; 
+        
     }
 }
---
+++ src.at.dej/algodat/main.java
@@ -0,0 +1,9 @@
+public class main {
+    
+    public static void main(String[] args) {
+       int[] test1 =  DataGenerator.generateDataArray(5);
+
+
+        System.out.println(test1);
+    }
+}
